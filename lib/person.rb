6c99bcd10d825bf56ea169e33d53e9f1058cd298
Romano Livi <romano_livi@outlook.com>
2020-12-23 18:28:25 +0000

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,11 +1,22 @@
 class Person 
-  attr_accessor :name, :birthday, :hair_color, :eye_color,
+  attr_accessor :name, :birthday, :hair_color,
   :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
   
   def initialize(attributes)
     attributes.each {|key, value| self.send(("#{key}="), value)}
   end
 end
+
+
+
+romano_attributes = {name: "Romano", height: "5'10"}
+
+romano = Person.new(romano_attributes)
+
+puts romano.name
+puts romano.height
+
+
 
 
 
